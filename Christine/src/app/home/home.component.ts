d51a72a4fd2ad80a6c335962c71d077df4f6bf5a
ChristineWeidhofer <xtine@gmx.at>
2023-02-20 15:07:22 +0100

    add styles

--- Christine/src/app/home/home.component.ts
+++ Christine/src/app/home/home.component.ts
@@ -9,4 +9,5 @@ export class HomeComponent {
   cardText: string = "Visit this special place with us! We will arrange an unforgettable travel experience for you!";
   btnText: string = "Book a tour!";
   btnLink: string = "/about";
+  cardPrice: number = 1200;
 }
