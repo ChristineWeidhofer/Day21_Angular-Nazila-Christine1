44ba9487173df0a707d12c3ef8f2210d1a1432ca
Nazila <azabdaftar@outlook.com>
2023-02-20 12:06:52 +0100

    Navbar and Hero Section layout

---
+++ Nazila/Classwork/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Nazila/Classwork/src/app/hero/hero.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss']
+})
+export class HeroComponent {
+
+}
