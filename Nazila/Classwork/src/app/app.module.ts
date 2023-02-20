4339ca0b16d550f51e8f1ff59fac483ed37e63c3
Nazila <azabdaftar@outlook.com>
2023-02-20 15:23:36 +0100

    rouelink added

--- Nazila/Classwork/src/app/app.module.ts
+++ Nazila/Classwork/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeroComponent
+    HeroComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
